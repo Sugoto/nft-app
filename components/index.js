ba2ca62ade07be93aa043959b9b8f05fc39ba039
Sugoto Basu <sugotobasu1@gmail.com>
2022-06-23 22:23:15 +0530

    added

--- components/index.js
+++ components/index.js
@@ -1,6 +1,22 @@
-import HomeHeader from "./HomeHeader.js";
-import FocusedStatusBar from "./FocusedStatusBar.js";
-import NFTCard from "./NFTCard.js";
-import { CircleButton, RectButton } from "./Button.js";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { RectButton, CircleButton } from "./Button";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+import DetailsBid from "./DetailsBid";
+import FocusedStatusBar from "./FocusedStatusBar";
+import DetailsDesc from './DetailsDesc';
 
-export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton };
+export {
+  HomeHeader,
+  NFTCard,
+  SubInfo,
+  People,
+  EndDate,
+  RectButton,
+  CircleButton,
+  EthPrice,
+  NFTTitle,
+  DetailsBid,
+  FocusedStatusBar,
+  DetailsDesc,
+};
